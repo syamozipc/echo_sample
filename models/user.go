37519ea8cc41264b2210791def6609d6bb940ad4
syamozipc <syamozipc@gmail.com>
2022-12-20 10:17:29 +0900

    booleanバリデーションを追加

--- models/user.go
+++ models/user.go
@@ -6,9 +6,10 @@ type User struct {
 	// is-messiはカスタムバリデーション
 	// uuidは不正な値がきたときにecho.Bindが失敗してエラーを返すので、バリデーションまでこない。string型にしてvalidateにuuidを追加し、詰め替え時にuuid型にする
 	// Null〇〇もBind失敗するので、ポインタ型でバリデーションし、domainなどへの詰め替え時にNull〇〇にする
-	Id     string  `query:"id" param:"id" json:"id" validate:"required,uuid" ja:"ID"`
-	Name   string  `query:"name" json:"name" validate:"required,is-messi" ja:"ユーザー名"`
-	Age    *int    `query:"age" json:"age" validate:"omitempty,number" ja:"年齢"`
-	Email  string  `query:"email" json:"email" validate:"required,email" ja:"メールアドレス"`
-	Gender *string `query:"gender" json:"gender" validate:"omitempty,oneof=男性 女性 その他" ja:"性別"`
+	Id       string  `query:"id" param:"id" json:"id" validate:"required,uuid" ja:"ID"`
+	Name     string  `query:"name" json:"name" validate:"required,is-messi" ja:"ユーザー名"`
+	Age      *string `query:"age" json:"age" validate:"omitempty,number" ja:"年齢"`
+	Email    string  `query:"email" json:"email" validate:"required,email" ja:"メールアドレス"`
+	Gender   *string `query:"gender" json:"gender" validate:"omitempty,oneof=男性 女性 その他" ja:"性別"`
+	IsActive *string `query:"active" json:"active" validate:"omitempty,boolean" ja:"アクティブ"`
 }
